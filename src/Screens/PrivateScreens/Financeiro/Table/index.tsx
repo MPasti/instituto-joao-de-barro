c789e02d892111e7ec85872509ed3a63f482260f
cadu1324 <caduzin11324@gmail.com>
2024-12-08 11:59:35 -0300

    feat: adding nota fiscal at financeiro

--- src/Screens/PrivateScreens/Financeiro/Table/index.tsx
+++ src/Screens/PrivateScreens/Financeiro/Table/index.tsx
@@ -7,6 +7,7 @@ interface Props {
     status: string;
     dueDate: string;
     bankStatement: string;
+    invoice: string; 
   };
   data: {
     name: string;
@@ -14,6 +15,7 @@ interface Props {
     dueDate: string,
     incoming?: string;
     outgoing?: string;
+    invoiceLink?: string;
   }[];
 }
 
@@ -27,6 +29,7 @@ const TableComponent: React.FC<Props> = ({header, data}) => {
           <th>{header.status}</th>
           <th>{header.dueDate}</th>
           <th>{header.bankStatement}</th>
+          <th>{header.invoice}</th>
         </tr>
       </thead>
       <tbody>
@@ -36,6 +39,15 @@ const TableComponent: React.FC<Props> = ({header, data}) => {
             <td>{item.status}</td>
             <td>{item.dueDate}</td>
             <td>{item.outgoing}</td>
+            <td>
+              {item.invoiceLink ? (
+                <a href={item.invoiceLink} target="_blank" rel="noopener noreferrer">
+                  Nota Fiscal
+                </a>
+              ) : (
+                'N/A'
+              )}
+            </td>
           </tr>
         ))}
       </tbody>
